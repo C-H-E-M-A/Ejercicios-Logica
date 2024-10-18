3ce5b926ce35e6ed640904a1c0fa933117543ef4
C-H-E-M-A <josuesincrz@gmail.com>
2024-10-18 13:11:11 -0600

    Delete js/Funciones directory

--- js/Funciones/Arrays/js/Ejercicio7.js
+++
@@ -1,8 +0,0 @@
-//Eliminar duplicados
-//Dado un arreglo de números, retorna un nuevo arreglo con los valores únicos (sin
-//duplicados).
-
-let numeros=[1,1,2,3,4,5,6,6,8,4,5,];
-const unico=[new set (numeros)];
-
-console.log(unico);
